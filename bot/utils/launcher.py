0a6bedc11877f60644bd8f53623e9b82315e2f2e
AlexKrut <65369825+AlexKrutoy@users.noreply.github.com>
2024-09-19 23:46:34 +0300

    Update launcher.py

--- bot/utils/launcher.py
+++ bot/utils/launcher.py
@@ -11,16 +11,20 @@ from bot.core.registrator import register_sessions
 from bot.utils.scripts import get_session_names, get_proxies
 
 banner = """
-
-TESTING SHIT YAHOOOO
-
+██████╗  ██████╗ ██████╗ ████████╗██████╗ ██████╗  ██████╗ ████████╗
+██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝╚════██╗██╔══██╗██╔═══██╗╚══██╔══╝
+██████╔╝██║   ██║██████╔╝   ██║    █████╔╝██████╔╝██║   ██║   ██║   
+██╔═══╝ ██║   ██║██╔══██╗   ██║    ╚═══██╗██╔══██╗██║   ██║   ██║   
+██║     ╚██████╔╝██║  ██║   ██║   ██████╔╝██████╔╝╚██████╔╝   ██║   
+╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═════╝  ╚═════╝    ╚═╝   
+                                                                    
 """
 
 options = """
 Select an action:
 
-    1. Create session
-    2. Run clicker
+    1. Run clicker
+    2. Create session 
 """
 
 
@@ -76,9 +80,9 @@ async def process() -> None:
                 action = int(action)
                 break
 
-    if action == 1:
+    if action == 2:
         await register_sessions()
-    elif action == 2:
+    elif action == 1:
         tg_clients = await get_tg_clients()
 
         await run_tasks(tg_clients=tg_clients)
